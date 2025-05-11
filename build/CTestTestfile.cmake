ea2f3fdc95ceff42bbf108881810c833a5a93678
NG KENJIN <ngkenjin0417@gmail.com>
2025-05-11 22:49:46 +0800

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/neow/robocup/src/nav
-# Build directory: /home/neow/robocup/src/nav/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
